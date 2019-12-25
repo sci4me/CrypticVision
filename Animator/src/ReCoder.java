9ff7073cfc854a6f67d3fcca0532e3267a276c8c
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2019-12-25 18:43:12 -0500

    Rename Transcoder to ReCoder

--- Animator/src/ReCoder.java
+++ Animator/src/ReCoder.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 
-public final class Transcoder {
+public final class ReCoder {
     public static void main(final String[] args) {
         final File dir = new File(args[0]);
         if(!dir.isDirectory()) throw new IllegalArgumentException(args[0] + " is not a directory!");
